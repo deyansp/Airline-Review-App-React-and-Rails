2c8a2102200582d06c6376fd8d990774d9fbd25f
Deyan <1704796@uad.ac.uk>
2021-06-16 22:14:44 +0100

    Update reviews_controller.rb

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -2,9 +2,9 @@ module  Api
     module V1
         class ReviewsController < ApplicationController
             protect_from_forgery with: :null_session
-            
+
             def create
-                review = Review.new(review_params)
+                review = airline.reviews.new(review_params)
 
                 if review.save
                     render json: ReviewSerializer.new(review).serialized_json
@@ -24,6 +24,11 @@ module  Api
             end
 
             private
+
+            def airline
+                @airline ||= Airline.find(params[:airline_id])
+            end
+
             def review_params
                 params.require(:review).permit(:title, :description, :score, :airline_id)
             end
