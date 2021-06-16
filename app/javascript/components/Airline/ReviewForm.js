0225d3515666342f3ae2278b70526030385e9103
Deyan <1704796@uad.ac.uk>
2021-06-16 22:45:40 +0100

    Update ReviewForm.js

--- app/javascript/components/Airline/ReviewForm.js
+++ app/javascript/components/Airline/ReviewForm.js
@@ -1,10 +1,18 @@
-import React from "react"
+import React, { Fragment } from "react"
 
-const Wrapper = styled.div``
+//const Wrapper = styled.div``
 
-const ReviewForm = () => {
+const ReviewForm = (props) => {
+    const ratingOptions = [5, 4, 3, 2, 1].map( (score, index) => {
+        return (
+            <Fragment>
+                <input type="radio" value={score} name="rating" onChange={console.log("selected:", score)} id={`rating=${score}`} />
+                <label></label>
+            </Fragment>
+        )
+    })
     return (
-        <div></div>
+        <div>{ratingOptions}</div>
     )
 }
 
