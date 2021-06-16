c66848279523632d8f261b379d0a269250f08551
Deyan <1704796@uad.ac.uk>
2021-06-16 01:49:32 +0100

    data is now fetched from the API with "axios" and displayed in the grid and Airline components

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,41 @@
-import React from "react"
-
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import Airline from "../Airlines/Airline"
 const Airlines = () => {
-    return (<div>Airlines#index view</div>)
+    const[airlines, setAirlines] = useState([])
+
+    useEffect(() => {
+        // Get all airlines from API
+        // Update airlines in component state
+
+        axios.get("/api/v1/airlines.json")
+        .then( (response) => {
+            setAirlines(response.data.data)
+        })
+        .catch( (response) => console.log(response))
+    }, [airlines.length]) // only use effect when the length changes
+    
+    const grid = airlines.map( item => {
+        return(
+            <Airline 
+            key={item.attributes.name}
+            attributes={item.attributes}></Airline>
+        )
+    })
+
+    return (
+        <div className="home">
+            <div className="header">
+                <h1>OpenFlights</h1>
+                <div className="subheader">
+                    Honest, unbiased airline reviews.
+                </div>
+            </div>
+                <div className="grid">
+                    {grid}
+                </div>
+        </div>
+    )
 }
 
 export default Airlines
